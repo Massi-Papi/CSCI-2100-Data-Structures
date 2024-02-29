d95ad296f078aae8337a727534ed20cb12f715b2
Massimo Evelti <99697083+Massi-Papi@users.noreply.github.com>
2024-02-28 18:18:13 -0600

    Update SortedList_Project.cpp

--- SortedList_Project.cpp
+++ SortedList_Project.cpp
@@ -1,123 +1,304 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class Node {
-public:
-    int data;
-    Node* next;
+template<class T>
+class LinkedList
+{
+  private:
+    //Stores the value and pointer to the next node in list 
+    struct Node
+    {
+      T data;
+      Node *next;
+      Node(T val):data(val), next(nullptr) {}
+    };
 
-    Node(int data) {
-        this->data = data;
-        this->next = nullptr;
-    }
-};
+    //Storing Location of head and tail
+    Node *head, *tail;
 
-class SortedList {
-private:
-    Node* head;
+    // Size of list
+    int _size;
 
-public:
-    SortedList() {
-        head = nullptr;
-    }
-
-    void add(int data) {
-        Node* newNode = new Node(data);
-
-        // If the list is empty, add the new node as the head of the list
-        if (head == nullptr) {
-            head = newNode;
-            return;
+    //Search Function
+    Node *search(T val)
+    {
+      Node *currentNode = head;
+      while(currentNode != nullptr)
+      {
+        if(currentNode->data == val)
+        {
+          return currentNode;
         }
-
-        // If the new node should be added at the beginning of the list,
-        // set it as the new head of the list
-        if (data < head->data) {
-            newNode->next = head;
-            head = newNode;
-            return;
-        }
-
-        // Otherwise, find the correct position for the new node and
-        // insert it there
-        Node* prev = head;
-        Node* curr = head->next;
-        while (curr != nullptr && curr->data < data) {
-            prev = curr;
-            curr = curr->next;
-        }
-        prev->next = newNode;
-        newNode->next = curr;
+        currentNode =currentNode->next;        
+      }
+      //Outputting an error
+      cerr << "No such element in the list \n";
+      return nullptr;
     }
 
-    void remove(int data) {
-        // If the list is empty, there is nothing to remove
-        if (head == nullptr) {
-            return;
-        }
-
-        // If the node to be removed is the head of the list,
-        // set the next node as the new head
-        if (head->data == data) {
-            Node* toDelete = head;
-            head = head->next;
-            delete toDelete;
-            return;
-        }
-
-        // Otherwise, search for the node to be removed and remove it
-        Node* prev = head;
-        Node* curr = head->next;
-        while (curr != nullptr && curr->data != data) {
-            prev = curr;
-            curr = curr->next;
-        }
-
-        // If the node was found, remove it
-        if (curr != nullptr) {
-            prev->next = curr->next;
-            delete curr;
-        }
+    //Checking back of list for value 
+    bool isIn(T val)
+    {
+      if(search(val) != nullptr)
+      {
+      return true;
+      }
+      return false;
     }
 
-    //Checks to see if integer is in list
-    bool contains(int data) {
-        Node* curr = head;
-        while (curr != nullptr && curr->data != data) {
-            curr = curr->next;
-        }
-        return curr != nullptr;
+    //Append Function
+    void append(T val)
+    {
+      Node *node = new Node(val);
+      _size++;
+      if (head == nullptr)
+      {
+        head = node;
+        tail = node;
+      }
+      else
+      {
+        tail->next = node;
+        tail = node;
+      }
+      
+    }
+    //Prepend Function
+    void prepend(T val)
+    {
+      Node *node = new Node(val);
+      _size++;
+      if (head ==nullptr)
+      {
+        head = node;
+        tail = node;
+      }
+      else
+      {
+        node->next = head;
+        head = node;
+      }
+    }
+
+  
+    // Insert function
+    void sortAndInsert(T newValue, T valueToBeAfter=NULL)
+    { 
+ 
+      if(head==nullptr || valueToBeAfter == NULL)
+      {
+        append(newValue);
+        return;
+      }
+
+      //Find location of value after pointer
+      Node *locationPtr = search(valueToBeAfter);      
+      if(locationPtr != nullptr)
+      {
         
-    }
-
-    // Prints out sorted list
-
-    void printList() {
-        Node* curr = this->head;
-        while (curr != nullptr) {
-            cout << curr->data << " ";
-            curr = curr->next;
+        if(locationPtr == tail)
+        {
+          append(newValue);
         }
-        cout << endl;
+      
+        else
+        {
+          Node *newNode = new Node(newValue);
+          _size++;
+          newNode->next = locationPtr->next;
+          locationPtr->next = newNode;
+        } 
+      }
+      else
+      {
+        //Outputting an error
+        cerr << "The value given to be inserted after is not present.\n" << endl;
+      }
     }
+
+  public: 
+
+    //Constructor
+    LinkedList(): head(nullptr), tail(nullptr), _size(0) {}
+    
+
+    ~LinkedList()
+    {
+      Node *temporary = nullptr;
+      while (head)
+      {
+        temporary = head;
+        head = head->next;
+        delete temporary;
+      }
+      head = nullptr;
+    }
+
+    // Size Function
+    int size(){
+      return _size;
+    }
+
+
+    // Print Function
+    void printValues()
+    {
+      Node *node =head;
+      while(node != nullptr)
+      {
+        cout << node->data <<endl;
+        node = node->next;
+      }
+    }
+    // Checking to see if it's on the list
+    string within(T val)
+    {
+      if(isIn(val)==true)
+      {
+        return "True";
+      }
+      else
+      {
+        return "False";
+      }
+    }
+
+    //Insert function
+    void insert(T val)
+    {
+      Node* previous = nullptr;
+      Node* temporary = head;
+
+      
+      if(temporary->data <= val)
+      {
+        prepend(val);
+        return;
+      }
+      
+      while(temporary != nullptr && temporary->data > val)
+      {
+        previous = temporary;
+        temporary = temporary->next;
+      }
+      if(temporary==nullptr)
+      {
+        append(val);
+        return;
+      }
+      else
+      {
+        sortAndInsert(val, previous->data);
+      }
+    }
+
+    //Remove Function
+    void removeValues(T val)
+    {
+      
+      Node *temporary = head;
+      Node *previous = nullptr;
+      
+      if(temporary!=nullptr && temporary->data == val)
+      {
+        head = temporary->next;
+        delete temporary;
+        _size--;
+        return;
+      }  
+    
+      while(temporary!=nullptr && temporary->data != val)
+      {
+        previous = temporary;
+        temporary = temporary->next;
+      }
+      if(temporary == nullptr)
+      {
+        return;
+      }
+      previous->next = temporary->next;
+      
+      if(previous->next == nullptr)
+      {
+        tail = previous;
+      }
+      delete temporary;
+      _size--;
+    }
+
+
+    //Test Function 1
+    void testFill10()
+    {
+      for(int i=10;i>0;i--)
+      {
+        append(i); 
+      }  
+
+    }
+
+    //Test Function 2
+    void testFill69()
+    {
+      for(int i=69;i>0;i--)
+      {
+        append(i); 
+      }  
+
+    }
+
+    //Test Function 3
+    void testFill50()
+    {
+      for(int i=50;i>0;i--)
+      {
+        append(i); 
+      }  
+
+    }
+    
 };
+
 
 int main() {
-    SortedList list;
 
-    // Add some elements to the list
-    list.add(5);
-    list.add(1);
-    list.add(3);
-    list.add(2);
-    list.add(69);
-    list.add(1000);
-    list.add(-8);
-    list.add(0);
-    list.printList();
-    list.remove(3);
-    list.printList();
-    list.remove(2);
-    list.printList();
-    return 0;
+  cout << endl<<endl;
+  LinkedList<int> listSoBig;
+  listSoBig.testFill10();
+  cout << listSoBig.size()<< endl;
+  cout << listSoBig.within(30)<< endl;
+  listSoBig.insert(69);
+  listSoBig.insert(34);
+  listSoBig.printValues();
+  cout << ' ' << "\n";
+  listSoBig.removeValues(69);
+  listSoBig.printValues();
+  cout << listSoBig.size() <<endl;
+
+  LinkedList<int> listSoBig2;
+  listSoBig2.testFill69();
+  cout << listSoBig2.size()<< endl;
+  cout << listSoBig2.within(100)<< endl;
+  listSoBig2.insert(78);
+  listSoBig2.insert(76);
+  listSoBig2.printValues();
+  cout << ' ' << "\n";
+  listSoBig2.removeValues(69);
+  listSoBig2.printValues();
+  cout << listSoBig2.size() << endl;
+
+  LinkedList<int> listSoBig3;
+  listSoBig3.testFill50();
+  cout << listSoBig3.size()<< endl << endl;
+  cout << listSoBig3.within(50)<< endl;
+  listSoBig3.insert(60);
+  listSoBig3.insert(250);
+  listSoBig3.printValues();
+  listSoBig3.removeValues(100);
+  listSoBig3.printValues();
+  cout << listSoBig3.size() <<endl;
+
+  return 0;
+  
 }
